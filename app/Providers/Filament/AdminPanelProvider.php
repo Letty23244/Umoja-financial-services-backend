d484b64ddebbe9b8b3e145e39c072c4e0b75f4c4
letty23244 <akulluleticia23@gmail.com>
2026-06-11 09:15:16 +0300

    added alogo on my dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,6 +35,13 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+
+            ->brandLogo(asset('assets/logo.png'))
+           ->brandDarkLogo(asset('assets/logo-white.png')) 
+            ->brandLogoHeight('2.5rem')
+
+            
+
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
